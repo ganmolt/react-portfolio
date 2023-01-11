550877247f3c2bbb01137f9a594015637c26f8a7
ganmolt <o.qanm.o@gmail.com>
2023-01-11 13:55:21 +0900

    :sparkles: かわいい

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AboutPage from './components/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import Maru from './components/Maru';
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
+
+        <Maru />
 
         <Routes>
           <Route path="/" element = {<HomePage />} />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import image1 from "../../Images/bg1.jpg";
 import profileImage from "../Images/magmag.jpg";
 
 // 制作物のアイコン
@@ -25,25 +24,25 @@ const HomePage = () => {
         ganmoltです。このサイトはreactを触るために作りました。
       </p>
 
-      <section class="page-section" id="services">
+      <section className="page-section" id="services">
         {/* 概要 */}
-        <div class="text-center info">
-          <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
-          <h3 class="section-subheading text-muted mb-5">
+        <div className="text-center info">
+          <h2 className="section-heading text-uppercase">PORTFOLIO</h2>
+          <h3 className="section-subheading text-muted mb-5">
             私が作った作品一覧です
           </h3>
         </div>
-        <div class="row text-center">
+        <div className="row text-center">
           {/* LinkHub */}
-          <div class="col-md-4 col-sm-6 service">
+          <div className="col-md-4 col-sm-6 service">
             {/* 画像部分 */}
-            <div class="img-area">
+            <div className="img-area">
               <a href="https://colink.jp/"><img src={img_linkhub} /></a>
             </div>
             {/* タイトル */}
-            <h4 class="my-3">LinkHub</h4>
+            <h4 className="my-3">LinkHub</h4>
             {/* 説明 */}
-            <p class="text-muted">
+            <p className="text-muted">
               「リンク集」の共有をするサイトです。
               2022年秋の技育展にて登壇しました。
               使用技術：Ruby on rails, AWS
@@ -51,24 +50,24 @@ const HomePage = () => {
           </div>
 
           {/* 精密検索 */}
-          <div class="col-md-4 col-sm-6 service">
-            <div class="img-area">
+          <div className="col-md-4 col-sm-6 service">
+            <div className="img-area">
               <a href="https://seimitsu-kensaku.herokuapp.com/"><img src={img_seimitsukensaku} /></a>
             </div>
-            <h4 class="my-3">精密検索</h4>
-            <p class="text-muted">
+            <h4 className="my-3">精密検索</h4>
+            <p className="text-muted">
               精密採点DX-Gのデータ閲覧、曲検索が同時にできるサービスです。
               使用技術: Ruby on Rails, Amazon S3, Heroku
             </p>
           </div>
 
           {/* ポートフォリオ */}
-          <div class="col-md-4 col-sm-6 service">
-            <div class="img-area">
+          <div className="col-md-4 col-sm-6 service">
+            <div className="img-area">
               <a href="https://github.com/ganmolt"><img src={img_portfolio} /></a>
             </div>
-            <h4 class="my-3">ポートフォリオ</h4>
-            <p class="text-muted">
+            <h4 className="my-3">ポートフォリオ</h4>
+            <p className="text-muted">
               ポートフォリオを作ってみました。react.jsを初めて触りました。
               使用技術: React.js, github pages
             </p>
@@ -79,7 +78,7 @@ const HomePage = () => {
       
       {/* StackShareのバッジを表示 */}
       <section id="stackshare">
-        <h2 class="title">StackShare</h2>
+        <h2 className="title">StackShare</h2>
         <a data-theme="dark" data-layers="1,2,3,4" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/8735dd74b0339208e9210ba4d6a7bd"></a>
         <Helmet>
           <script async src="https://cdn1.stackshare.io/javascripts/client-code.js" charset="utf-8"></script>
@@ -88,7 +87,7 @@ const HomePage = () => {
 
 
       <section id="contact">
-        <h2 class="title">Contact</h2>
+        <h2 className="title">Contact</h2>
         <a href="https://github.com/ganmolt"><FaGithub size={'50px'} /></a>
       </section>
     </div>
