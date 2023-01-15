386c9548b679b78b50b19c6e9d0382f823a0e138
ganmolt <o.qanm.o@gmail.com>
2023-01-16 01:32:31 +0900

    :art: 画像の大きさを調整 :art: counterを追加

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import profileImage from "../Images/magmag.jpg";
+import profileImage from "../Images/profile.jpg";
 
 // 制作物のアイコン
-import img_seimitsukensaku from "../Images/seimitsukensaku.png";
+import img_seimitsukensaku from "../Images/seimitsukensaku.jpg";
 import img_linkhub from "../Images/linkhub.jpg";
 import img_portfolio from "../Images/magmag.jpg";
 
@@ -18,11 +18,40 @@ const HomePage = () => {
     <div className="container text-center">
       <h1>ganmoltのポートフォリオ</h1>
 
-      <img src={profileImage} className="profileImage" />
+      <img src={profileImage} className="profileImage" alt="profileImage" />
 
       <p>
         ganmoltです。このサイトはreactを触るために作りました。
       </p>
+
+      <div className="counter">
+        <table border="0">
+          <tbody>
+            <tr>
+              <td valign="middle">
+                <img src="http://smart-counter.net/images/today.gif" alt="アクセスカウンター" border="0"/>
+              </td>
+              <td valign="middle">
+                <img src="http://smart-counter.net/tday-d50-f5-008/63c3ef6240560/" alt="アクセスカウンター" border="0"/>
+              </td>
+
+              <td valign="middle">
+              <img src="http://smart-counter.net/images/yesterday.gif" alt="アクセスカウンター"/>
+              </td>
+              <td valign="middle">
+                <img src="http://smart-counter.net/yday-d50-f5-008/63c3ef6240560/" alt="アクセスカウンター" border="0"/>
+              </td>
+
+              <td valign="middle">
+                <img src="http://smart-counter.net/images/total.gif" alt="アクセスカウンター"/>
+              </td>
+              <td valign="middle">
+                <img src="http://smart-counter.net/total-d50-f6-008/63c3ef6240560/" alt="アクセスカウンター" border="0"/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
       <section className="page-section" id="services">
         {/* 概要 */}
@@ -37,7 +66,7 @@ const HomePage = () => {
           <div className="col-md-4 col-sm-6 service">
             {/* 画像部分 */}
             <div className="img-area">
-              <a href="https://colink.jp/"><img src={img_linkhub} /></a>
+              <a href="https://colink.jp/"><img src={img_linkhub} alt="LinkHub" /></a>
             </div>
             {/* タイトル */}
             <h4 className="my-3">LinkHub</h4>
@@ -45,6 +74,8 @@ const HomePage = () => {
             <p className="text-muted">
               「リンク集」の共有をするサイトです。
               2022年秋の技育展にて登壇しました。
+            </p>
+            <p className="textmuted">
               使用技術：Ruby on rails, AWS
             </p>
           </div>
@@ -52,11 +83,13 @@ const HomePage = () => {
           {/* 精密検索 */}
           <div className="col-md-4 col-sm-6 service">
             <div className="img-area">
-              <a href="https://seimitsu-kensaku.herokuapp.com/"><img src={img_seimitsukensaku} /></a>
+              <a href="https://seimitsu-kensaku.herokuapp.com/"><img src={img_seimitsukensaku} alt="seimitsu-kensaku" /></a>
             </div>
             <h4 className="my-3">精密検索</h4>
             <p className="text-muted">
               精密採点DX-Gのデータ閲覧、曲検索が同時にできるサービスです。
+            </p>
+            <p className="textmuted">
               使用技術: Ruby on Rails, Amazon S3, Heroku
             </p>
           </div>
@@ -64,11 +97,13 @@ const HomePage = () => {
           {/* ポートフォリオ */}
           <div className="col-md-4 col-sm-6 service">
             <div className="img-area">
-              <a href="https://github.com/ganmolt"><img src={img_portfolio} /></a>
+              <a href="https://github.com/ganmolt"><img src={img_portfolio} alt="portfolio" /></a>
             </div>
             <h4 className="my-3">ポートフォリオ</h4>
             <p className="text-muted">
               ポートフォリオを作ってみました。react.jsを初めて触りました。
+            </p>
+            <p className="textmuted">
               使用技術: React.js, github pages
             </p>
           </div>
