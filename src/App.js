a884888e71c8803219115d1c198efdd9aa465491
ganmolt <o.qanm.o@gmail.com>
2023-12-21 19:09:21 +0900

    更新作業等

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -18,7 +17,6 @@ function App() {
 
         <Routes>
           <Route path="/" element = {<HomePage />} />
-          {/* <Route path="/about" element = {<AboutPage />} /> */}
         </Routes>
 
         <Footer />
