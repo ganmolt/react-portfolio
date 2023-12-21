dc93aad9c38867ebdc9bdbfa751b0c1fd4a50918
ganmolt <o.qanm.o@gmail.com>
2023-12-21 22:28:43 +0900

    refactoring: homeページ回り

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import './App.css';
-import { HomePage } from './components/HomePage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from './pages/home';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-
 import { Maru } from './components/Maru';
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
-
         <Maru />
 
         <Routes>
-          <Route path="/" element = {<HomePage />} />
+          <Route path="/" element = {<Home />} />
         </Routes>
 
         <Footer />
---
+++ src/pages/home/parts/contact.jsx
@@ -0,0 +1,10 @@
+import { FaGithub } from "react-icons/fa"
+
+export const Contact = () => {
+  return (
+    <>
+      <h2 className="title">Contact</h2>
+      <a href="https://github.com/ganmolt"><FaGithub size={'50px'} /></a>
+    </>
+  )
+}
