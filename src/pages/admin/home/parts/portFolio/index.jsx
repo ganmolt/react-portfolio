5f65d9ab0c36df5e2a1960be427fac879c8eeeda
ganmolt <o.qanm.o@gmail.com>
2024-01-22 23:11:13 +0900

    backendからwork取得

--- src/pages/admin/home/parts/portFolio/index.jsx
+++ src/pages/admin/home/parts/portFolio/index.jsx
@@ -1,28 +1,66 @@
+import { createContext, useState, useEffect } from 'react'
+
+import axios from "axios";
+import Cookies from "js-cookie";
+
 import imgSeimitsuKensaku from "Images/home/SeimitsuKensaku.jpg";
 import imgLinkHub from "Images/home/LinkHub.jpg";
 import imgPortfolio from "Images/home/Slugma.jpg";
+
+import { ImageEncoder } from "./imageEncoder";
 import { Work } from "./work";
 
 export const PortFolio = () => {
   const title = "PORTFOLIO";
   const description = "私が作った作品一覧です";
+
+  const [loading, setLoading] = useState();
+  const [works, setWorks] = useState();
+
+  const handleGetWorks = async () => {
+    try {
+      if (!Cookies.get("_access_token")) return;
+    
+      const res = await axios.get("http://localhost:3001/admin/works", {
+        headers: {
+          "access-token": Cookies.get("_access_token"),
+        },
+      });
+      if (res?.status === 200) {
+        setWorks(res?.data);
+      } else {
+        console.log("no Data");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    handleGetWorks();
+  }, [setWorks])
+
   return (
     <>
+      <ImageEncoder />
       <div className="text-center info">
         <h2 className="section-heading">{title}</h2>
         <h3 className="section-subheading text-muted mb-5">{description}</h3>
       </div>
       <div className="row text-center">
-        <div className="col-md-4 col-sm-6 service">
+        {works?.map((work, idx) => (
+          <div key={idx} className="col-md-4 col-sm-6 service">
           <Work
-            name="LinkHub"
-            url="https://colink.jp"
-            img={imgLinkHub}
-            description="「リンク集」の共有をするサイトです。2022年秋の技育展にて登壇しました。"
-            tech="Ruby on rails, AWS"
+            name={work.name}
+            url={work.url}
+            encodedImg={work.encodedImg}
+            description={work.description}
+            tech={work.tech}
           />
         </div>
-        <div className="col-md-4 col-sm-6 service">
+        ))}
+        
+        {/* <div className="col-md-4 col-sm-6 service">
           <Work
             name="精密検索"
             url="https://seimitsu-kensaku.com/"
@@ -38,8 +76,8 @@ export const PortFolio = () => {
             img={imgPortfolio}
             description="ポートフォリオを作ってみました。react.jsを初めて触りました。"
             tech="React, github pages"
-          />
-        </div>
+          /> */}
+        {/* </div> */}
       </div>
     </>
   )
